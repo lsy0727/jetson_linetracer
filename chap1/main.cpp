7b1de9bde52b1ca8bbd17d6de86aa1aedc4b6a94
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2024-11-21 21:50:55 +0900

    Update main.cpp

--- chap1/main.cpp
+++ chap1/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 using namespace cv;
 
 int main() {
-    VideoCapture source("/home/linux/lsy/vscode_ws/simulation/8_lt_cw_100rpm_in.mp4");
+    VideoCapture source("/home/linux/lsy/vscode_ws/simulation/7_lt_ccw_100rpm_in.mp4");
     if (!source.isOpened()) {
         cerr << "video open failed!" << endl;
         return -1;
@@ -12,7 +12,7 @@ int main() {
     int delay = cvRound(1000 / fps);
 
     bool first_run = true;
-    Point tmp_point;
+    Point tmp_pt, prev_pt;
     Mat frame, gray, thresh, result, stats, centroids;
     int error;
 
@@ -22,16 +22,16 @@ int main() {
 
         // 첫 실행시 기준점 설정
         if (first_run) {
-            tmp_point = Point(thresh.cols / 2, thresh.rows - 1);
+            tmp_pt = Point(thresh.cols / 2, thresh.rows - 1);
             first_run = false;
         }
 
         // 객체 찾기
-        findObjects(thresh, tmp_point, result, stats, centroids);  // stats와 centroids 추가
+        findObjects(thresh, tmp_pt, prev_pt, result, stats, centroids);  // stats와 centroids 추가
         // 객체 그리기
-        drawObjects(result, stats, centroids, tmp_point, result);  // stats와 centroids 추가
+        drawObjects(result, stats, centroids, tmp_pt, result);  // stats와 centroids 추가
         // error 계산
-        error = getError(result, tmp_point);
+        error = getError(result, prev_pt);
         cout << "error : " << error << endl;
 
         // 결과 출력
