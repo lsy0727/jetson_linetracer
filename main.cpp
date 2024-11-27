8a94c41bce55139aa74b27207fd8501774e4e8a2
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2024-11-27 11:24:48 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,56 +1,36 @@
-#include "vision.hpp"
+#include <sys/time.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <signal.h>
+#include "vision.hpp"
+#include "dxl.hpp"
 using namespace std;
 using namespace cv;
 
-bool ctrl_c_pressed;
-void ctrlc(int)
-{
-    ctrl_c_pressed = true;
-}
+bool ctrl_c_pressed= false;
+void ctrlc_handler(int) { ctrl_c_pressed= true; }
 int main() {
-    string src = "nvarguscamerasrc sensor-id=0 ! \
-        video/x-raw(memory:NVMM), width=(int)640, height=(int)360, \
-        format=(string)NV12, framerate=(fraction)30/1 ! \
-        nvvidconv flip-method=0 ! video/x-raw, \
-        width=(int)640, height=(int)360, format=(string)BGRx ! \
-        videoconvert ! video/x-raw, format=(string)BGR ! appsink";
-    VideoCapture source(src, CAP_GSTREAMER);
-    if (!source.isOpened()) { cerr << "video open failed!" << endl; return -1; }
+    VideoCapture source("/home/linux/lsy/vscode_ws/simulation/7_lt_ccw_100rpm_in.mp4");
+    if (!source.isOpened()) {
+        cerr << "video open failed!" << endl;
+        return -1;
+    }
 
-    string dst1 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
-        nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
-        h264parse ! rtph264pay pt=96 ! \
-        udpsink host=203.234.58.164 port=8001 sync=false";
-    VideoWriter writer1(dst1, 0, (double)30, Size(640, 360), true);
-    if (!writer1.isOpened()){ cerr << "Writer open failed!" << endl; return -1; }
-
-    string dst2 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
-        nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
-        h264parse ! rtph264pay pt=96 ! \
-        udpsink host=203.234.58.164 port=8002 sync=false";
-    VideoWriter writer2(dst2, 0, (double)30, Size(640, 360), false);
-    if (!writer2.isOpened()) { cerr << "Writer open failed!" << endl; return -1;}
-
-    string dst3 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
-        nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
-        h264parse ! rtph264pay pt=96 ! \
-        udpsink host=203.234.58.164 port=8003 sync=false";
-    VideoWriter writer3(dst3, 0, (double)30, Size(640, 360), false);
-    if (!writer3.isOpened()) { cerr << "Writer open failed!" << endl; return -1;}
-
-    // 첫 실행
     bool first_run = true;
-
+    Point tmp_pt;
     Mat frame, gray, thresh, result, stats, centroids;
-    Point tmp_pt, prev_pt;
-
     int error;
+
+    Dxl mx;
+    if(!mx.open()) { cout <<"dynamixel open error"<<endl; return-1; }
+    bool control = false;
+
     // 시간 관련 변수
     struct timeval start, end1;
-    double diff1;
+    double time1;
+    int vel1 = 0, vel2 = 0;
+    // 시그널
+    signal(SIGINT, ctrlc_handler);
 
     while (true) {
         gettimeofday(&start,NULL);  //시작
@@ -63,26 +43,43 @@ int main() {
             tmp_pt = Point(thresh.cols / 2, thresh.rows - 1);
             first_run = false;
         }
-
+        
         // 객체 찾기
-        findObjects(thresh, tmp_pt, prev_pt, result, stats, centroids);  // stats와 centroids 추가
+        findObjects(thresh, tmp_pt, result, stats, centroids);  // stats와 centroids 추가
         // 객체 그리기
         drawObjects(stats, centroids, tmp_pt, result);  // stats와 centroids 추가
         // error 계산
-        error = getError(result, prev_pt);
+        error = getError(result, tmp_pt);
 
+        if (mx.kbhit()) // 키보드입력체크
+        {
+            char c = mx.getch();    // 키입력받기
+            if (c == 's') {
+                vel1 = 100;
+                vel2 = 100;
+                control = true;
+            }
+        }
+
+        // 모터 제어**************************************************************************
+        if (control) {
+            vel1 = vel1-error;
+            vel2 = -(vel2+error);
+            mx.setVelocity(vel1, vel2);
+        }   //********************************************************************************
+
+        if (ctrl_c_pressed) break;  //ctrl+c 입력시 탈출
         usleep(30*1000);
+
         gettimeofday(&end1,NULL);   //종료시간
-        diff1=end1.tv_sec + end1.tv_usec/1000000.0 - start.tv_sec - start.tv_usec/1000000.0;
-        cout << "error : " << error << "\ttime : " << diff1 << endl;
+        time1=end1.tv_sec + end1.tv_usec/1000000.0 - start.tv_sec - start.tv_usec/1000000.0;
+        cout << "error : " << error << "\tvel1:" << vel1 << ',' << "\tvel2:" << vel2 << "\ttime : " << time1 << endl;
 
         // 결과 출력
-        writer1 << frame;
-        writer2 << gray;
-        writer3 << thresh;
-
-        if (ctrl_c_pressed) break; 
+        imshow("frame", frame);
+        // imshow("gray", gray);
+        imshow("thresh", result);
     }
-
+    mx.close();
     return 0;
 }
