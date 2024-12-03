c17fe10008568a9095b55729a9b66723a896d8a7
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2024-12-03 14:23:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,11 +41,11 @@ int main() {
 
     bool first_run = true;
     Point tmp_pt;
-    Mat frame, gray, thresh, result, stats, centroids;
+    Mat frame, gray, thresh, stats, centroids;
     int error;
 
     struct timeval start, end1;
-    double diff1;
+    double diff;
 
     // 시그널
     signal(SIGINT, ctrlc_handler);
@@ -54,7 +54,7 @@ int main() {
     if(!mx.open()) { cout <<"dynamixel open error"<<endl; return-1; }
     bool mode =false;
     double lval = 0, rval = 0;
-    double k = 0.5;
+    double k = 0.2;
 
     while (true) {
         gettimeofday(&start,NULL);  //시작
@@ -69,11 +69,11 @@ int main() {
         }
 
         // 객체 찾기
-        findObjects(thresh, tmp_pt, result, stats, centroids);  // stats와 centroids 추가
+        findObjects(thresh, tmp_pt, stats, centroids);  // stats와 centroids 추가
         // 객체 그리기
-        drawObjects(stats, centroids, tmp_pt, result);  // stats와 centroids 추가
+        drawObjects(stats, centroids, tmp_pt, thresh);  // stats와 centroids 추가
         // error 계산
-        error = getError(result, tmp_pt);
+        error = getError(thresh, tmp_pt);
 
         if(mx.kbhit()) // 없으면 제어 멈춤
         {
@@ -83,22 +83,20 @@ int main() {
         }
         if (ctrl_c_pressed) break;  //ctrl+c 입력시 탈출
 
-        if (100-k*error < 0) lval = 100 - k* error;
-        else lval = -10;
-        if (100-k*error > 0) rval = -(100 + k* error);
-        else rval = 10;
-        if(mode) mx.setVelocity(leftvel, rightvel);
+        lval = 100 - k* error;
+        rval = -(100 + k* error);
+        if(mode) mx.setVelocity(lval, rval);
 
         usleep(30*1000);
 
         gettimeofday(&end1,NULL);   //종료시간
-        diff1=end1.tv_sec + end1.tv_usec/1000000.0 - start.tv_sec - start.tv_usec/1000000.0;
-        cout << "error : " << error << "\tlval : " << lval << "\trval : " << rval << "\ttime : " << diff1 << endl;
+        diff=end1.tv_sec + end1.tv_usec/1000000.0 - start.tv_sec - start.tv_usec/1000000.0;
+        cout << "error : " << error << "\tlval : " << lval << "\trval : " << rval << "\ttime : " << diff << endl;
 
         // 결과 출력
         writer1 << frame;
         writer2 << gray;
-        writer3 << result;
+        writer3 << thresh;
     }
     mx.close();
     return 0;
