819f01449085ab5e399b3f60363e92996e3e23cf
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2024-12-04 14:21:47 +0900

    Update vision.hpp

--- chap1/vision.hpp
+++ chap1/vision.hpp
@@ -2,14 +2,14 @@
 #define _VISION_HPP_
 
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
 
 using namespace std;
 using namespace cv;
 
 void preprocess(VideoCapture& source, Mat& frame, Mat& gray, Mat& thresh);
 void findObjects(Mat& thresh, Point& tmp_pt, Mat& stats, Mat& centroids);
-void drawObjects(const Mat& stats, const Mat& centroids, const Point& tmp_pt, Mat& thresh);
-int getError(const Mat& thresh, const Point& tmp_pt);
+void drawObjects(Mat& stats, Mat& centroids, Point& tmp_pt, Mat& thresh);
+int getError(Mat& thresh, Point& tmp_pt);
 
 #endif //_VISION_HPP_
